a6c08554946b64e7281505c79762f095a6c5f257
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-08-02 12:39:24 +0100

    Add the user role

--- helpers/jwtHelpers.js
+++ helpers/jwtHelpers.js
@@ -6,14 +6,24 @@ function authorize() {
   return expressJwt({
     secret,
     algorithms: ['HS256'],
+    isRevoked,
   }).unless({
     path: [
       `${api}/auth/login`,
       `${api}/auth/register`,
       { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      {
+        url: /\/api\/v1\/categories(.*)/,
+        methods: ['GET', 'OPTIONS'],
+      },
     ],
   });
+}
+
+async function isRevoked(req, token) {
+  if (!token.payload.isAdmin) {
+    return true;
+  }
 }
 
 module.exports = authorize;
