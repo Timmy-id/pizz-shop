a8fd922c1eaec07bb0818d773683cf0fcf1b8213
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-08-02 12:49:45 +0100

    Add the delete user and get user count feature

--- features/user/userController.js
+++ features/user/userController.js
@@ -36,4 +36,51 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers, getUser };
+const deleteUser = async (req, res) => {
+  const userId = req.params.userId;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'invalid id' });
+  }
+
+  const user = await User.findById(userId);
+
+  try {
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'user not found' });
+    } else {
+      await user.deleteOne();
+      return res.status(200).json({
+        success: true,
+        message: 'user deleted successfully',
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const getUsersCount = async (req, res) => {
+  try {
+    const usersCount = await User.countDocuments();
+
+    if (!usersCount) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'invalid request' });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: usersCount,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { getAllUsers, getUser, deleteUser, getUsersCount };
--- features/user/userRoute.js
+++ features/user/userRoute.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { getAllUsers, getUser } = require('./userController');
+const {
+  getAllUsers,
+  getUser,
+  deleteUser,
+  getUsersCount,
+} = require('./userController');
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:userId', getUser);
+router.delete('/:userId', deleteUser);
+router.get('/get/count', getUsersCount);
 
 module.exports = router;
